c547a22478c943527dce81bba78a868fd041d84d
Kunal Saha <kunalsaha1107@gmail.com>
2026-06-22 17:48:57 +0530

    Add TDD_Loggin_Week1 module and exercise 1,3 and 4

---
+++ TDD_Loggin_Week1/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calc = new Calculator();
+        assertEquals(15, calc.multiply(3, 5));
+    }
+}
