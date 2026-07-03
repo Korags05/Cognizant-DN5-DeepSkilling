93fe58d821d48f0d6594d5709fa3efc513e0324c
Kunal Saha <kunalsaha1107@gmail.com>
2026-07-03 19:43:59 +0530

    SpringCore and maven - exercise2

--- SpringCore_Maven_Week2/src/main/java/org/example/service/BookService.java
+++ SpringCore_Maven_Week2/src/main/java/org/example/service/BookService.java
@@ -1,9 +1,18 @@
 package org.example.service;
 
+import org.example.repository.BookRepository;
+
 public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public void serviceMethod() {
         System.out.println("Book Service Running...");
+        bookRepository.displayBook();
     }
 
 }
