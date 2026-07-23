53c374cae72b8c7cb1407698f19b072f4760ba41
Kunal Saha <kunalsaha1107@gmail.com>
2026-07-23 20:12:48 +0530

    Add Course List feature with course enrollment functionality (hands on 2)

--- Angular/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Angular/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,9 +1,56 @@
 import { Component } from '@angular/core';
+import { CourseCard } from '../../components/course-card/course-card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+  courses = [
+    {
+      id: 1,
+      name: 'Angular',
+      code: 'CS101',
+      credits: 4,
+    },
+
+    {
+      id: 2,
+      name: 'Java',
+      code: 'CS102',
+      credits: 3,
+    },
+
+    {
+      id: 3,
+      name: 'Spring Boot',
+      code: 'CS103',
+      credits: 4,
+    },
+
+    {
+      id: 4,
+      name: 'Database',
+      code: 'CS104',
+      credits: 3,
+    },
+
+    {
+      id: 5,
+      name: 'Operating System',
+      code: 'CS105',
+      credits: 4,
+    },
+  ];
+
+  selectedCourseId = 0;
+
+  onEnroll(id: number) {
+    console.log('Enrolling in', id);
+
+    this.selectedCourseId = id;
+  }
+}
